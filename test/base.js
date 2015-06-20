f7eb2a124e1b6de881f1ee64a03db7f508ee7688
ajlopez <webmaster@ajlopez.com>
2015-06-20 11:39:40 -0300

    Store and fetch value by key

--- test/base.js
+++ test/base.js
@@ -12,4 +12,23 @@ exports['create base'] = function (test) {
         base = data;
         test.done();
     });
-}
+};
+
+exports['store and fetch value'] = function (test) {
+    test.async();
+    
+    base.store({ bucket: 'bucket1', key: 'adam', value: { name: 'Adam', age: 800 } }, function (err, data) {
+        test.equal(err, null);
+        
+        base.fetch({ bucket: 'bucket1', key: 'adam' }, function (err, data) {
+            test.equal(err, null);
+            test.ok(data);
+            test.ok(Array.isArray(data));
+            test.equal(data.length, 1);
+            var obj = data[0];
+            test.equal(obj.name, 'Adam');
+            test.equal(obj.age, 800);
+            test.done();
+        });
+    });
+};
