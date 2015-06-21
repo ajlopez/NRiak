ebd5ff005824975cbc833f0901eea4b7a9f1f4ff
ajlopez <webmaster@ajlopez.com>
2015-06-21 07:17:17 -0300

    Refactor test to use simpleasync

--- test/base.js
+++ test/base.js
@@ -1,5 +1,6 @@
 
 var nriak = require('..');
+var async = require('simpleasync');
 
 var base;
 
@@ -17,19 +18,25 @@ exports['create base'] = function (test) {
 exports['store and fetch value'] = function (test) {
     test.async();
     
-    base.store({ bucket: 'bucket1', key: 'adam', value: { name: 'Adam', age: 800 } }, function (err, data) {
-        test.equal(err, null);
-        
-        base.fetch({ bucket: 'bucket1', key: 'adam' }, function (err, data) {
-            test.equal(err, null);
-            test.ok(data);
-            test.ok(Array.isArray(data));
-            test.equal(data.length, 1);
-            var obj = data[0];
-            test.equal(obj.name, 'Adam');
-            test.equal(obj.age, 800);
-            test.done();
-        });
-    });
+    async()
+    .then(function (data, next) {
+        base.store({ bucket: 'bucket1', key: 'adam', value: { name: 'Adam', age: 800 } }, next);
+    })
+    .then(function (data, next) {
+        base.fetch({ bucket: 'bucket1', key: 'adam' }, next);
+    })
+    .then(function (data, next) {
+        test.ok(data);
+        test.ok(Array.isArray(data));
+        test.equal(data.length, 1);
+        var obj = data[0];
+        test.equal(obj.name, 'Adam');
+        test.equal(obj.age, 800);
+        test.done();
+    })
+    .fail(function (err) {
+        test.fail(err);
+    })
+    .run();
 };
 
